37d8ccd7e568694647bcee8c5536827a03dee806
MatrixXx C <mathieu.charon87@gmail.com>
2020-12-04 08:52:52 +0100

    test

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Entity\Contact;
 use App\Form\CommentType;
+use App\Form\ContactType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\HttpFoundation\Request;
+use App\Notification\ContactNotification;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,12 +22,28 @@ use App\Form\ArticleType;
 
 class ContactController extends AbstractController
 {
+
     /**
      * @Route("/contact", name="contact")
+     * @return Response
      */
-    public function index(): Response
+    public function index (Request $request, ContactNotification $notification): Response
     {
-        return $this->render('contact.html.twig');
+
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification->notify($contact);
+            $this->addFlash('success', 'Votre email a bien été envoyé');
+            return $this->redirectToRoute('home');
+        }
+
+            return $this->render('contact.html.twig', [
+            'form'         => $form->createView()
+        ]);
     }
 
 }
